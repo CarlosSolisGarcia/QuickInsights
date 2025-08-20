2ddb8c37e12b3f58295163c33f866dda573bcf83
Carlos Solis <csolisgarcia@live.com>
2025-08-20 23:19:46 +0200

    20/08/2025: Added format to the variables, added parameters to the global EDA

--- main.py
+++ main.py
@@ -17,12 +17,26 @@ if uploaded_file is not None:
     st.dataframe(data=dataframe.head())
 
     ## EDA
+    ### Setting up the statistics to show:
+    dataframe_num_rows = dataframe.shape[0]
+    dataframe_num_cols = dataframe.shape[1]
+    dataframe_num_missing_values = dataframe.isna().sum().sum()
+    dataframe_percentage_missing_values = dataframe.isna().sum().sum() / dataframe.size * 100
+    dataframe_num_duplicates = dataframe.duplicated().sum()
+    dataframe_percentage_duplicates = dataframe_num_duplicates / dataframe_num_rows * 100
+
     st.subheader("📊 Exploratory Data Analysis")
     st.markdown(
         f"""
             #### Dataset shape:
-            - **Number of observations (rows)**: {dataframe.shape[0]}
-            - **Number of variables (columns)**: {dataframe.shape[1]} 
+            - **Number of observations (rows)**: {dataframe_num_rows}
+            - **Number of variables (columns)**: {dataframe_num_cols}
+            #### Missing values:
+            - Number of missing values: {dataframe_num_missing_values}
+            - % of missing values: {dataframe_percentage_missing_values:.2f} %
+            #### Duplicates:
+            - Number of duplicates: {dataframe_num_duplicates}
+            - % of duplicate rows: {dataframe_percentage_duplicates:.2f} % 
         """
                 )
 
@@ -37,8 +51,9 @@ if uploaded_file is not None:
     # ¿Hay filas duplicadas? (núm y % sobre total) --> Indicar cómo generar las claves para el recuento
 
     columns = dataframe.columns
-    selected_variable = st.segmented_control(label="Variable",
-                          options = columns)
+    selected_variable = st.segmented_control(label="Select a variable to preview",
+                                             options=columns,
+                                             key="Variable_previsualization_selector")
 
     ### Descripción de la variable:
     # Tipo de dato:
@@ -46,33 +61,61 @@ if uploaded_file is not None:
 
         series_to_analyze = dataframe[selected_variable]
 
-        data_info_tab, data_chart_tab = st.tabs(["Data info", "Chart"])
+        data_info_tab, histogram_tab = st.tabs(["Data info", "Histogram"])
 
         with data_info_tab:
             col1, col2 = st.columns(2)
 
             with col1:
                 st.subheader("General info")
-                st.write(f"Data type: ", series_to_analyze.dtype)
-                st.write(f"Missing cells: ", pd.isna(series_to_analyze).sum())
-                st.write(f"Missing cells %: ", pd.isna(series_to_analyze).sum() / dataframe.shape[0] * 100)
-                st.write(f"Memory usage (kB): ", series_to_analyze.memory_usage() / 1024)
+                st.write(f"Data type: {series_to_analyze.dtype}")
+                st.write(f"Missing values: {pd.isna(series_to_analyze).sum()}")
+                st.write(f"Missing values %: {pd.isna(series_to_analyze).sum() / dataframe.shape[0] * 100:.2f}")
+                st.write(f"Memory usage (kB):  {series_to_analyze.memory_usage() / 1024:.2f} kb")
 
             with col2:
                 st.subheader("Statistical info")
-                st.write(f"Mean: ", series_to_analyze.mean())
-                st.write(f"Median: ", series_to_analyze.median())
-                st.write(f"Standard deviation: ", series_to_analyze.std())
-                st.write(f"Max: ", series_to_analyze.max())
-                st.write(f"Min: ", series_to_analyze.min())
+                st.write(f"Mean: {series_to_analyze.mean():.2f}")
+                st.write(f"Median: {series_to_analyze.median()}")
+                st.write(f"Standard deviation: {series_to_analyze.std():.2f}")
+                st.write(f"Max: {series_to_analyze.max()}")
+                st.write(f"Min: {series_to_analyze.min()}")
 
-        with data_chart_tab:
+        with histogram_tab:
 
             fig, ax = plt.subplots()
             ax.hist(series_to_analyze, bins = 20)
+            ax.set_title(str(selected_variable))
             st.pyplot(fig)
 
     st.divider()
 
-    st.write(dataframe.describe(include="all"))
+    st.subheader("🎯📌 Target and feature selection")
+
+    target_variable = st.segmented_control("Select the target variable",
+                                           options=columns,
+                                           key="Target_variable_selector",
+                                           selection_mode="single")
+
+    if target_variable is not None:
+
+        y_data = dataframe[target_variable]
+
+        filtered_options = list(columns).copy()
+        filtered_options.remove(target_variable)
+        explored_variables = st.segmented_control("Select the variables to explore",
+                                                  options = filtered_options,
+                                                  selection_mode="multi")
+
+        if explored_variables:
+            X_data = dataframe[explored_variables]
+            #st.write(X_data.head())
+
+            ### correlations
+            corr_matrix = X_data.corr()
+            fig, ax = plt.subplots()
+            sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+            st.pyplot(fig)
+
+
     # st.write(dataframe.dtypes)
